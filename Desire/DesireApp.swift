6c648d96a18f195cff5964aeb4ab422a0dadcf1c
Mankong <mankong@mankong.me>
2026-07-03 18:05:54 +0800

    Initial Commit

---
+++ Desire/DesireApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesireApp.swift
+//  Desire
+//
+//  Created by mankong on 2026/7/3.
+//
+
+import SwiftUI
+
+@main
+struct DesireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

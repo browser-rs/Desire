17744b68be7325e625c9909a7de5c4cb7b0334a6
Mankong <mankong@mankong.me>
2026-07-04 18:01:00 +0800

    feat: 内容拦截器扩展至 50+ 条规则
    覆盖 Google Ads/Analytics、Meta/Facebook、Microsoft/LinkedIn、
    Amazon、Twitter/X、TikTok、主流广告网络及分析服务
    
    规则改用代码生成，消除重复和维护开销

--- Desire/Features/ContentBlocking/ContentBlocker.swift
+++ Desire/Features/ContentBlocking/ContentBlocker.swift
@@ -48,58 +48,44 @@ class ContentBlocker: ObservableObject {
         }
     }
 
-    private let blockRulesJSON = """
-    [{
-        "trigger": { "url-filter": ".*doubleclick.net/.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*googlesyndication.com/.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*googleadservices.com/.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*google-analytics.com/.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*googletagmanager.com/.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*googletagservices.com/.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*facebook.com/tr.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*doubleclick.net.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*adservice.google.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*pagead2.googlesyndication.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*adsystem.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*adnxs.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*criteo.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*casalemedia.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*rubiconproject.com.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*openx.net.*" },
-        "action": { "type": "block" }
-    },{
-        "trigger": { "url-filter": ".*pubmatic.com.*" },
-        "action": { "type": "block" }
-    }]
-    """
+    private let blockRulesJSON = {
+        let trackers = [
+            // Google / Alphabet
+            "doubleclick.net", "googlesyndication.com", "googleadservices.com",
+            "google-analytics.com", "googletagmanager.com", "googletagservices.com",
+            "adservice.google.com", "pagead2.googlesyndication.com", "googleads.g.doubleclick.net",
+            "www.googletagmanager.com", "connect.facebook.net", "ad.doubleclick.net",
+            "static.doubleclick.net", "td.doubleclick.net", "googleoptimize.com",
+            // Meta / Facebook
+            "facebook.com/tr", "facebook.net", "fbcdn.net", "connect.facebook.net",
+            "pixel.facebook.com", "an.facebook.com", "atdmt.com",
+            // Microsoft / LinkedIn
+            "bat.bing.com", "c.bing.com", "ads.microsoft.com",
+            "linkedin.com/px", "px.ads.linkedin.com",
+            // Amazon
+            "amazon-adsystem.com", "aax.amazon-adsystem.com", "amazonadsi.com",
+            // Twitter / X
+            "analytics.twitter.com", "ads-twitter.com", "t.co",
+            // TikTok
+            "ads.tiktok.com", "analytics.tiktok.com",
+            // Major ad networks
+            "adsystem.com", "adnxs.com", "criteo.com", "casalemedia.com",
+            "rubiconproject.com", "openx.net", "pubmatic.com",
+            "thetradedesk.com", "adsrvr.org", "adzerk.net",
+            "lijit.com", "sovrn.com", "indexww.com", "quantserve.com",
+            "scorecardresearch.com", "comscore.com", "moatads.com",
+            // Analytics
+            "hotjar.com", "mouseflow.com", "fullstory.com",
+            "crazyegg.com", "clicktale.net", "optimizely.com",
+            "segment.io", "segment.com", "amplitude.com", "mixpanel.com",
+            "heap.com", "branch.io", "adjust.com", "appsflyer.com",
+        ]
+
+        let rules = trackers.enumerated().map { i, domain in
+            """
+            {"trigger":{"url-filter":".*\(domain.replacingOccurrences(of: "/", with: "\\\\/")).*"},"action":{"type":"block"}}
+            """
+        }
+        return "[\(rules.joined(separator: ","))]"
+    }()
 }
